bb9098957985e72ef043ab252f0479b5f65232d0
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-10-17 22:33:37 +0530

    Updated

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@ import { TopNav } from '@/components';
 </script>
 
 <template>
-  <TopNav />
-  <section>
-    <router-view />
-  </section>
+  <div class="m-4">
+    <TopNav />
+    <div class="container mx-auto px-4 py-6">
+      <section class="container">
+        <router-view />
+      </section>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios'
 
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
 const app = createApp(App)
 
